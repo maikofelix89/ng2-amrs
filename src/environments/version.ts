081ba8d099a93b040548f38fb2a03778657a5506
maikofelix89 <maikofelix89@gmail.com>
2018-09-06 10:44:00 +0300

    Bump 2.10.0

---
+++ src/environments/version.ts
@@ -0,0 +1,23 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "v2.1.0-861-gaf3e0dec-dirty",
+    "hash": "gaf3e0dec",
+    "distance": 861,
+    "tag": "v2.1.0",
+    "semver": {
+        "raw": "v2.1.0",
+        "major": 2,
+        "minor": 1,
+        "patch": 0,
+        "prerelease": [],
+        "build": [],
+        "version": "2.1.0"
+    },
+    "suffix": "861-gaf3e0dec-dirty",
+    "semverString": "2.1.0+861.gaf3e0dec",
+    "version": "2.9.0",
+    "buildDate": "2018-09-06T07:36:54.729Z"
+};
+/* tslint:enable */
